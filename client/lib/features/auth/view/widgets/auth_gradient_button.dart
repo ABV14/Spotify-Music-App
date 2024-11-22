fa631459f5ef304553f70258887469edd59fca0f
Abhivarma Birru <abhivarma.b@quadranttechnologies.com>
2024-11-21 20:23:28 -0800

    Login Page and Sign Up page design complete

--- client/lib/features/auth/view/widgets/auth_gradient_button.dart
+++ client/lib/features/auth/view/widgets/auth_gradient_button.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:client/core/theme/app_pallette.dart';
 
 class AuthGradientButton extends StatelessWidget {
-  const AuthGradientButton({super.key});
+  final String buttonText;
+  final VoidCallback onTap;
+  const AuthGradientButton(
+      {super.key, required this.buttonText, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
@@ -15,13 +18,13 @@ class AuthGradientButton extends StatelessWidget {
         borderRadius: BorderRadius.circular(7),
       ),
       child: ElevatedButton(
-        onPressed: () {},
+        onPressed: onTap,
         style: ElevatedButton.styleFrom(
             fixedSize: const Size(395, 55),
             backgroundColor: Pallete.transparentColor,
             shadowColor: Pallete.transparentColor),
-        child: const Text("Sign Up",
-            style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600)),
+        child: Text(buttonText,
+            style: const TextStyle(fontSize: 17, fontWeight: FontWeight.w600)),
       ),
     );
   }
--- client/lib/features/auth/view/widgets/custom_field.dart
+++ client/lib/features/auth/view/widgets/custom_field.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
 
 class CustomField extends StatelessWidget {
-  final String hintText; 
-  const CustomField({super.key,required this.hintText});
+  final String hintText;
+  final TextEditingController controller;
+  final bool isObsureText;
+  const CustomField(
+      {super.key,
+      required this.hintText,
+      required this.controller,
+      this.isObsureText = false});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      decoration:InputDecoration(
-        hintText:hintText,
-      )
-    );
+        controller: controller,
+        obscureText: isObsureText,
+        validator: (val) {
+          if (val!.trim().isEmpty) {
+            return "$hintText is missing";
+          }
+          return null;
+        },
+        decoration: InputDecoration(
+          hintText: hintText,
+        ));
   }
 }
