941c2ee9db096d4506ece4fd440f3e40dec2ea5c
Abhivarma Birru <abhivarma.b@quadranttechnologies.com>
2024-11-23 23:53:01 -0800

    client-authentication-integration

---
+++ client/lib/core/failure/failure.dart
@@ -0,0 +1,5 @@
+class AppFailure {
+  final String message;
+  // final int statusCode;
+  AppFailure([this.message = "Sorry, An unexpected error has occured"]);
+}
--- client/lib/features/auth/view/pages/signup_page.dart
+++ client/lib/features/auth/view/pages/signup_page.dart
@@ -1,7 +1,10 @@
 import 'package:client/core/theme/app_pallette.dart';
+import 'package:client/features/auth/repositories/auth_remote_repository.dart';
+import 'package:client/features/auth/view/pages/login_page.dart';
 import 'package:client/features/auth/view/widgets/auth_gradient_button.dart';
 import 'package:client/features/auth/view/widgets/custom_field.dart';
 import 'package:flutter/material.dart';
+import 'package:fpdart/fpdart.dart';
 
 class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
@@ -21,8 +24,8 @@ class _SignupPageState extends State<SignupPage> {
     nameController.dispose();
     emailController.dispose();
     passwordController.dispose();
-    super.dispose();
     formKey.currentState!.validate();
+    super.dispose();
   }
 
   @override
@@ -59,21 +62,38 @@ class _SignupPageState extends State<SignupPage> {
                   isObsureText: true),
               const SizedBox(height: 20),
               AuthGradientButton(
-                onTap: () {},
+                onTap: () async {
+                  final response = await AuthRemoteRepository().signup(
+                      name: nameController.text,
+                      email: emailController.text,
+                      password: passwordController.text);
+                  final result = switch (response) {
+                    Left(value: final l) => l,
+                    Right(value: final r) => r.toString()
+                  };
+                  print(result);
+                },
                 buttonText: "Sign Up",
               ),
               const SizedBox(height: 20),
-              RichText(
-                  text: TextSpan(
-                      text: "Already have an account?",
-                      style: Theme.of(context).textTheme.titleMedium,
-                      children: const [
-                    TextSpan(
-                        text: " Sign In",
-                        style: TextStyle(
-                            color: Pallete.gradient2,
-                            fontWeight: FontWeight.bold))
-                  ]))
+              GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const LoginPage()));
+                  },
+                  child: RichText(
+                      text: TextSpan(
+                          text: "Already have an account?",
+                          style: Theme.of(context).textTheme.titleMedium,
+                          children: const [
+                        TextSpan(
+                            text: " Sign In",
+                            style: TextStyle(
+                                color: Pallete.gradient2,
+                                fontWeight: FontWeight.bold))
+                      ])))
             ]),
           ),
         ));
