5fcf9b0ebb68edef5e5bf444080efad83f3b0067
Abhivarma Birru <abhivarma.b@quadranttechnologies.com>
2024-11-26 21:38:40 -0800

    song-upload-commit-1

--- client/lib/core/widgets/custom_field.dart
+++ client/lib/core/widgets/custom_field.dart
@@ -2,17 +2,22 @@ import 'package:flutter/material.dart';
 
 class CustomField extends StatelessWidget {
   final String hintText;
-  final TextEditingController controller;
+  final TextEditingController? controller;
   final bool isObsureText;
+  final bool readOnly;
+  final VoidCallback? onTap;
   const CustomField(
       {super.key,
       required this.hintText,
       required this.controller,
-      this.isObsureText = false});
+      this.isObsureText = false,
+      this.readOnly = false,
+      this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+        onTap: onTap,
         controller: controller,
         obscureText: isObsureText,
         validator: (val) {
