2d6a66539179ecdc5dab2e9db09870d884c04196
Abhivarma Birru <abhivarma.birru@gmail.com>
2024-11-29 01:20:41 -0800

    song-player-and-upload-song-completed

--- client/lib/features/auth/view/pages/home_page.dart
+++ client/lib/features/auth/view/pages/home_page.dart
@@ -2,13 +2,25 @@ import 'package:client/core/providers/current_user_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomePage extends ConsumerWidget {
+class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
-
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<HomePage> createState() => _HomePageState();
+}
+  class _HomePageState extends ConsumerState<HomePage>{
+  int selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
     final user = ref.watch(currentUserNotifierProvider);
     print(user);
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(
+            icon: Image.asset('assets/images/home_filled.png'), label: 'Home'),
+        BottomNavigationBarItem(
+            icon: Image.asset('assets/images/library.png'), label: 'Library'),
+      ]),
+    );
   }
 }
+
